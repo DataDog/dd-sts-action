05de54ff207790ea476ab2976160cefe0965ad5a
Julien Doutre <julien.doutre@datadoghq.com>
2026-02-09 15:28:27 +0100

    remove last mentions of mit

--- post.js
+++ post.js
@@ -1,5 +1,5 @@
 /*
-* Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+* Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 * Copyright 2026-present Datadog, Inc.
 */
 
